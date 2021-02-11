90948d750ae3eedee363e47bedec8201135e8498
charlespereira1 <charles.spereira-ext@viavarejo.com.br>
2021-02-10 23:08:18 -0300

    commit touch id

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, TouchableHighlight, Alert} from 'react-native';
+import TouchID from 'react-native-touch-id';
 
 const App: React.FC = () => {
+  const [supported, setSupported] = useState(false);
+
+  const handleLogin = () => {
+    console.log('eu');
+  };
+
+  useEffect(() => {
+    TouchID.isSupported()
+      .then((sucess) => {
+        setSupported(true);
+      })
+      .catch((error) => {
+        console.log('Error' + error);
+        Alert.alert('Touch ID não suportado/habilitado');
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
+      <TouchableHighlight style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableHighlight>
       <Text style={styles.text}>Hello word</Text>
     </View>
   );
@@ -17,6 +38,16 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 16,
+    fontWeight: 'bold',
+  },
+  button: {
+    borderRadius: 3,
+    marginBottom: 15,
+    padding: 15,
+    backgroundColor: '#7159c1',
+  },
+  buttonText: {
+    color: '#fff',
     fontWeight: 'bold',
   },
 });
